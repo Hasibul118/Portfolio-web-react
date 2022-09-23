7181b2e8e103176cd5fb76d0c71971f14c672485
Hasibul Hasan <hasibshanto85@gmail.com>
2022-09-23 14:12:16 +0600

    navbar full done

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -1,16 +1,40 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './navigation.css'
 
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
 import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
 import {RiServerLine} from 'react-icons/ri'
 
+function useScrollDirection() {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+      if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection); // add event listener
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection); // clean up
+    }
+  }, [scrollDirection]);
+
+  return scrollDirection;
+};
+
 const Navigation = () => {
 
   const [activeNav, setActiveNav] = useState('#')
+  const scrollDirection = useScrollDirection();
 
   return (
-    <nav>
+    <nav className={`${ scrollDirection === "down" ? "bottom-24" : "bottom-0"} h-24 bg-blue-200`}>
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
