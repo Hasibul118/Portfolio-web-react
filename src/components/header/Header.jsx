f394e4a2307b4df70fa32a6615fa3a7437a88cf8
Hasibul Hasan <hasibshanto85@gmail.com>
2022-09-27 17:56:49 +0600

    assects added, responsive, ui

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css'
 
 import CTA from './CTA'
-import ME from '../../assets/me.png'
+import ME from '../../assets/profile.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -13,7 +13,7 @@ const Header = () => {
 
         <h5>Hello I'm</h5>
         <h1>Hasibul Hasan</h1>
-        <h5 className="text-light">Software Developer</h5>
+        <h5 className="text-white">Software Developer</h5>
         <CTA />
         <HeaderSocials />
         
